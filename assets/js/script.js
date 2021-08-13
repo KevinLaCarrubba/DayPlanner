1483ae3e07f73f73af54af4d074dfc129f32fd0e
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-12 23:11:48 -0400

    set us HTML rows and colums. put time IDs

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+//Create our document ready function to make sure nothing runs before we load the page.
+// $(document).ready(function() {
+//all code goes in here to make sure that the page is fullly loaded before it runs anything at all.
+
+//listen for click events on the page
+// onClick function()
+//on click event gets fired grab the saved event options
+//save user inputs and time to a variable
+var time = "";
+var input = "";
+//after values are grabbed save to localStorage
+
+//display alert to user that items have been saved to localStorage
+
+//create some kind of timeout value that removes the notification
+
+//function to update time
+// get the current time with moment
+
+// create loop to go over all the time blocks
+//apply them to the correct - id
+
+//if else if statement
+//check to see if we have passed our time
+// check out removeClass and addClass
+
+//check to see if current hour matches physical time
+
+//check to see if time is future
+
+// retrieve localStorage and render items to the correct row
+
+/* DONT WASTE TIME GOING DOWN THE HOLE
+_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+_¶¶___________________________________¶¶
+_¶¶___________________________________¶¶
+__¶¶_________________________________¶¶_
+__¶¶_________________________________¶¶_
+___¶¶_______________________________¶¶__
+___¶¶______________________________¶¶___
+____¶¶¶__________________________¶¶¶____
+_____¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶_____
+_______¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶_______
+_________¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶_________
+___________¶¶¶¶¶_¶¶¶¶¶¶¶_¶¶¶¶___________
+______________¶¶¶¶_¶¶¶_¶¶¶______________
+________________¶¶¶_¶_¶¶________________
+_________________¶¶¶_¶¶_________________
+__________________¶¶_¶¶_________________
+__________________¶¶_¶__________________
+__________________¶¶_¶¶_________________
+________________¶¶¶_¶_¶¶¶_______________
+_____________¶¶¶¶¶__¶__¶¶¶¶¶____________
+__________¶¶¶¶¶_____¶_____¶¶¶¶__________
+________¶¶¶¶________¶_______¶¶¶¶¶_______
+_______¶¶¶__________¶__________¶¶¶¶_____
+_____¶¶¶____________¶____________¶¶¶____
+____¶¶¶_____________¶______________¶¶___
+___¶¶¶______________¶_______________¶¶__
+___¶¶_______________¶________________¶¶_
+__¶¶________________¶________________¶¶_
+__¶¶_______________¶¶¶________________¶_
+__¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶
+__¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶
+__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ */
+
+// }
