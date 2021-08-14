25cc3a5c35e44a6036692e2e35c2be2fe9ed39fa
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-14 19:02:19 -0400

    rre did all html in js. added to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,123 @@
-//Create our document ready function to make sure nothing runs before we load the page.
-$(document).ready(function () {
-  //all code goes in here to make sure that the page is fullly loaded before it runs anything at all.
-  var currentDay = moment().format("dddd MMM Do, YYYY");
-  $("#currentDay").text(currentDay);
-  var saveBtn = $(".saveBtn");
-  var textInput = $(".text-input");
-  var textBox9 = $("#plan-9");
-  var textBox10 = $("#plan-10");
-  var textBox11 = $("#plan-11");
-  var textBox12 = $("#plan-12");
-  var textBox1 = $("#plan-1");
-  var textBox2 = $("#plan-2");
-  var textBox3 = $("#plan-3");
-  var textBox4 = $("#plan-4");
-  var textBox5 = $("#plan-5");
+//Create array for time objects
+var timeSlots = [
+    {time: '9AM' ,timeBlock:9},
+    {time: '10AM' ,timeBlock:10},
+    {time: '11AM',timeBlock:11},
+    {time: '12AM',timeBlock:12},
+    {time: '1PM',timeBlock:13},
+    {time: '2PM',timeBlock:14},
+    {time: '3PM',timeBlock:15},
+    {time: '4PM',timeBlock:16},
+    {time: '5PM',timeBlock:17},
+]
+//set variable for moment
+var moment=moment();
+//set variable for current day
+var currentDay = document.getElementById('currentDay')
+//set variable for display container
+var plannedDisplay = document.querySelector(".container")
+//add current day text content to HTML page
+currentDay.textContent = moment.format("dddd MMM Do, YYYY")
 
-  //I want to be able to click the button
-  //and save the input text within the row of said button
-  //to local storage for that row
+function saveMessage(){
+    var msg = document.getElementById('notification')
+    msg.style.display = "block";
+    setTimeout(function(){
+        msg.style.display = "none";
+    },1200);
+}
+//funciton to display the planner
+function renderPlanner () {
+    //loop through array of objects
+    timeSlots.forEach((item) => {
+        console.log(item)
+        console.log(item.timeBlock)
+        //set current time variable
+        var currentTime = moment.format('H');
+        //parseInt current time
+        currentTime = parseInt(currentTime);
+    //create a bootstrap row with class row and id with timeblock
+    var rowDiv = document.createElement('div')
+    rowDiv.setAttribute('id',"time-"+item.timeBlock)
+    rowDiv.classList.add("row")
 
-  saveBtn.click(saveText);
-
-  function saveText() {
-    event.preventDefault();
-    saveText = textInput.value;
-    localStorage.setItem("Text", saveText);
-  }
-
-  var textTimeBlock = [
-    { textField: textBox9, timeBlock: 9 },
-    { textField: textBox10, timeBlock: 10 },
-    { textField: textBox11, timeBlock: 11 },
-    { textField: textBox12, timeBlock: 12 },
-    { textField: textBox1, timeBlock: 13 },
-    { textField: textBox2, timeBlock: 14 },
-    { textField: textBox3, timeBlock: 15 },
-    { textField: textBox4, timeBlock: 16 },
-    { textField: textBox5, timeBlock: 17 },
-  ];
-
-  function currentTime() {
-    // get the current time
-    var currentTime = moment().format("H");
-    //set the current time to an int for math comparison
-    currentTime = parseInt(currentTime);
-    //for loop to loop through textTimeBlock
-    for (var i = 0; i < textTimeBlock.length; i++) {
-      //if currentTime is === timblock number add class present
-      if (currentTime === textTimeBlock[i].timeBlock) {
-        textTimeBlock[i].textField.addClass("present");
-      } else if (currentTime > textTimeBlock[i].timeBlock) {
-        textTimeBlock[i].textField.removeClass("present");
-        textTimeBlock[i].textField.removeClass("future");
-        textTimeBlock[i].textField.addClass("past");
-      } else {
-        textTimeBlock[i].textField.removeClass("present");
-        textTimeBlock[i].textField.removeClass("past");
-        textTimeBlock[i].textField.addClass("future");
-      }
+    //create a div for time
+    var timeDiv = document.createElement('div')
+    //add bootstrap col classes and hour
+    timeDiv.classList.add('col-12', 'col-md-1', 'col-lg-1', 'hour')
+    //display time 
+    timeDiv.textContent = item.time;
+    //append timeDiv to rowDiv
+    rowDiv.appendChild(timeDiv);
+    
+    //create a textarea
+    var textArea = document.createElement('textarea');
+    //set id to text area
+    textArea.setAttribute('id','plan-'+item.timeBlock)
+    //add bootstrao col classes and description 
+    textArea.classList.add('col-12', 'col-md-10', 'col-lg-10', 'description')
+    //check if current time is equal to textarea timeblock and assaign present class
+    if (currentTime === item.timeBlock) {
+        textArea.classList.add('present');
+    } 
+    //check if time has passed and add past class
+    else if (currentTime > item.timeBlock) {
+        textArea.classList.add('past')
+    } else {
+    //add future class to all other text areas
+        textArea.classList.add('future')
     }
-  }
-  currentTime();
+    //appened text area to rowDiv
+    rowDiv.appendChild(textArea);
 
-  // retrieve localStorage and render items to the correct row
+    //create a var to make a button
+    var submitBtn = document.createElement('button');
+    //add id to button from timeblock
+    submitBtn.setAttribute('id','subBtn-'+item.timeBlock)
+    //add bootstrap col classes and saveBtn class
+    submitBtn.classList.add('col-12', 'col-md-1', 'col-lg-1', 'saveBtn')
+    //add textContent to button
+    submitBtn.textContent = 'Submit'
+    //append button to rowDIv
+    rowDiv.appendChild(submitBtn)
+    //append rowDiv to display containers
+    plannedDisplay.appendChild(rowDiv)
+}
+);;buttonID();
+getPlan();
+};
 
-  /* DONT WASTE TIME GOING DOWN THE HOLE
-_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
-_¶¶___________________________________¶¶
-_¶¶___________________________________¶¶
-__¶¶_________________________________¶¶_
-__¶¶_________________________________¶¶_
-___¶¶_______________________________¶¶__
-___¶¶______________________________¶¶___
-____¶¶¶__________________________¶¶¶____
-_____¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶_____
-_______¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶_______
-_________¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶_¶¶¶¶_________
-___________¶¶¶¶¶_¶¶¶¶¶¶¶_¶¶¶¶___________
-______________¶¶¶¶_¶¶¶_¶¶¶______________
-________________¶¶¶_¶_¶¶________________
-_________________¶¶¶_¶¶_________________
-__________________¶¶_¶¶_________________
-__________________¶¶_¶__________________
-__________________¶¶_¶¶_________________
-________________¶¶¶_¶_¶¶¶_______________
-_____________¶¶¶¶¶__¶__¶¶¶¶¶____________
-__________¶¶¶¶¶_____¶_____¶¶¶¶__________
-________¶¶¶¶________¶_______¶¶¶¶¶_______
-_______¶¶¶__________¶__________¶¶¶¶_____
-_____¶¶¶____________¶____________¶¶¶____
-____¶¶¶_____________¶______________¶¶___
-___¶¶¶______________¶_______________¶¶__
-___¶¶_______________¶________________¶¶_
-__¶¶________________¶________________¶¶_
-__¶¶_______________¶¶¶________________¶_
-__¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶
-__¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶¶
-__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶ */
-});
+renderPlanner();
+
+function buttonID(){
+    timeSlots.forEach((item) =>{
+        var buttonId = document.getElementById
+        ('subBtn-'+item.timeBlock)
+        buttonId.addEventListener('click', savePlan);
+    })
+}
+function savePlan() {
+    timeSlots.forEach((item) =>{ 
+        var getId = document.getElementById("plan-"+item.timeBlock)
+        var planName = getId.getAttribute('id')
+        var getPlan = getId.value;
+        if(getPlan !== ''){
+        localStorage.setItem(planName, getPlan)
+        saveMessage();
+        }
+        if(getPlan === ''){
+            localStorage.removeItem(planName)
+        }
+    })
+}
+
+function getPlan() {
+    timeSlots.forEach((item) =>{ 
+        var getId = document.getElementById("plan-"+item.timeBlock)
+        var planName = getId.getAttribute('id')
+        var savedPlan = localStorage.getItem(planName)
+        console.log(savedPlan)
+        getId.textContent = savedPlan
+})
+}
+
